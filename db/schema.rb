abead56bbdbaca7495d0a3e8956c6255d1619c98
kajcyk <kajcyk@gmail.com>
2018-12-09 22:09:01 +0100

    Add relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_170148) do
+ActiveRecord::Schema.define(version: 2018_12_09_210404) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2018_11_28_170148) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
